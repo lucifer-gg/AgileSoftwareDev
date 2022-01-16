39e30ffd2157134dcd8a674f81de1e2744746675
lucifer-gg <1140737321@qq.com>
2022-01-16 11:22:36 +0800

    初始化spring框架

---
+++ AgileBackEnd/src/main/java/com/example/AgileBackEnd/AgileBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgileBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileBackEndApplication.class, args);
+	}
+
+}
---
+++ AgileBackEnd/src/test/java/com/example/AgileBackEnd/AgileBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgileBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
